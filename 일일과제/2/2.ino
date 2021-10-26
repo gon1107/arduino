225ac5e30d19ac5cc809345f84346c2d901d89d8
DESKTOP-5GA4I66\hyosung_daily <gon_1107@naver.com>
2021-10-26 17:21:14 +0900

    다이얼 금고 random 함수로 구현

--- 일일과제/2/2.ino
+++ 일일과제/2/2.ino
@@ -1,9 +1,45 @@
-void setup() {
-  // put your setup code here, to run once:
+//일일과제
 
+const int led[6] = {3, 5, 6, 9, 10, 11};
+const int analogPin = A0;
+int count = 0;
+
+void setup() {
+  Serial.begin(115200);
+  randomSeed(1024);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  randomSeed(1024);
+  int randNumber = random (683, 341);
+  int randNumber2 = random (1, 341);
+  int randNumber3 = random (342, 341);
 
+  int sensorInput = analogRead(analogPin);
+//  Serial.println(sensorInput);
+  if(sensorInput==randNumber && count==0){
+    analogWrite(led[0], 255);
+    count++;
+  }
+
+  if(sensorInput==randNumber2 && count==1){
+    analogWrite(led[1], 255);
+    count++;
+  }
+  if(sensorInput==randNumber3 && count==2){
+    analogWrite(led[2], 255);
+    count++;
+    for(int n=0;n<=2;n++)
+      analogWrite(led[n], 0);
+    for(int i=0;i<=2;i++){
+      for(int n=0;n<=5;n++){
+        analogWrite(led[n], 255);
+        delay(200);
+      }
+      for(int n=5;n>=0;n--){
+        analogWrite(led[n], 0);
+        delay(200);
+      }
+    }
+  }
 }

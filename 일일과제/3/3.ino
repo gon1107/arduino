6331f6c633b658e3f1d815efb7662e7ef2738f4e
DESKTOP-5GA4I66\hyosung_daily <gon_1107@naver.com>
2021-10-28 12:29:49 +0900

    코드 재사용성 개선 리팩토링

--- 일일과제/3/3.ino
+++ 일일과제/3/3.ino
@@ -1,10 +1,23 @@
 //일일과제
 
+bool check = false;
 int count = 0;
 
 const int ACTIVE_BUZZER = 9;
 const int BUZZER = 10;
 const int button[4] = {2, 3, 4, 5};
+
+const char *questions[2] = {
+  "문제 1) 다음 중 컴퓨터의 기능이 아닌 것은?\n",
+  "문제 2) 기록되어 있는 자료를 읽기만 가능한 기억 장치는?\n",
+};
+
+const char *options[2] = {
+  "1. 출력기능\t2. 입력기능\n3. 연산기능\t4. 판단기능\n",
+  "1. 롬(ROM)\t2. 램(RAM)\n3. 하드디스크\t4. 모니터\n",
+};
+
+const int answers[2] = {4, 3};
 
 char note[] = "ggaaggeggeed ggaaggegedec ";
 char beat[] = "11111121111221111112111122";
@@ -27,68 +40,70 @@ int duration(char beat){
   return beat - '0';
 }
 
+void quiz(int number){
+  if(digitalRead(button[0])==HIGH){
+    check = true;
+    if (answers[count] == 1){
+      count++;
+    }
+  }
+  else if(digitalRead(button[1])==HIGH){
+    check = true;        
+    if (answers[count] == 2){
+      count++;        
+    } 
+  }
+  else if(digitalRead(button[2])==HIGH){
+    check = true;        
+    if (answers[count] == 3){      
+      count++;       
+    }
+  }
+  else if(digitalRead(button[3])==HIGH){
+    check = true;        
+    if (answers[count] == 4){
+      count++;          
+    }   
+  }
+
+  if(count == number && check == true){
+    check = false;
+
+    digitalWrite(ACTIVE_BUZZER, HIGH);
+    delay(3000);
+    digitalWrite(ACTIVE_BUZZER, LOW);
+  }
+  else if(count == number + 1){
+    check = false;        
+    Serial.print(questions[count]);
+    Serial.print(options[count]);  
+            
+    for(int i=0;i<note_length;i++){
+      if(note[i] != ' '){
+        tone(BUZZER, freq(note[i]));  
+      }
+      delay(tempo*duration(beat[i]));
+      
+      noTone(BUZZER);
+      delay(100);
+    }
+  }
+}
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  delay(1000);
+  
   pinMode(ACTIVE_BUZZER, OUTPUT);
 
   for(int n=0;n<=3;n++){
     pinMode(button[n], INPUT);
   }
-  Serial.println("문제 1) 다음 중 컴퓨터의 기능이 아닌 것은?");
-  Serial.println("1. 출력기능  2. 입력기능");
-  Serial.println("3. 연산기능  4. 판단기능");
+  
+  Serial.print(questions[count]);
+  Serial.print(options[count]);
 }
 
 void loop() {
-  if(count == 0){
-    if(digitalRead(button[0])==HIGH || digitalRead(button[1])==HIGH || digitalRead(button[2])==HIGH){
-      digitalWrite(ACTIVE_BUZZER, HIGH);
-      delay(3000);
-      digitalWrite(ACTIVE_BUZZER, LOW);
-    }
-    else if(digitalRead(button[3])==HIGH){
-      count++;
-
-      Serial.println("문제 1) 다음 중 컴퓨터 언어가 아닌 것은?");
-      Serial.println("1. python  2. java");
-      Serial.println("3. c--  4. c++");
-      
-      for(int i=0;i<note_length;i++){
-        if(note[i] != ' '){
-          tone(BUZZER, freq(note[i]));  
-        }
-        delay(tempo*duration(beat[i]));
-        
-        noTone(BUZZER);
-        delay(100);
-      }
-    }
-  }
-  
-  if(count == 1){
-    if(digitalRead(button[0])==HIGH || digitalRead(button[1])==HIGH || digitalRead(button[3])==HIGH){
-    
-      digitalWrite(ACTIVE_BUZZER, HIGH);
-      delay(3000);
-      digitalWrite(ACTIVE_BUZZER, LOW);
-    }
-    else if(digitalRead(button[2])==HIGH){
-      count=0;
-
-      Serial.println("문제 1) 다음 중 컴퓨터의 기능이 아닌 것은?");
-      Serial.println("1. 출력기능  2. 입력기능");
-      Serial.println("3. 연산기능  4. 판단기능");
-
-      for(int i=0;i<note_length;i++){
-        if(note[i] != ' '){
-          tone(BUZZER, freq(note[i]));  
-        }
-        delay(tempo*duration(beat[i]));
-        
-        noTone(BUZZER);
-        delay(100);
-      }
-    }
-  }
+  quiz(count);
 }

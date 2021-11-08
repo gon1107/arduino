dfa379a368b29f37ee46d9ee8df40ac449206084
DESKTOP-5GA4I66\hyosung_daily <gon_1107@naver.com>
2021-11-08 17:15:30 +0900

    4자리 7 세그먼트 코드 작성

---
+++ 5장/524_0/524_0.ino
@@ -0,0 +1,78 @@
+//p.299 카운터 만들기
+
+const unsigned int led[7] = {2, 3, 4, 5, 6, 7, 8};
+const unsigned int com[1+4] = {-1, 9, 10, 11, 12};
+
+const unsigned int num[10][7] = {
+  { 1, 1, 1, 1, 1, 1, 0 }, /* 0 */
+  { 0, 1, 1, 0, 0, 0, 0 }, /* 1 */
+  { 1, 1, 0, 1, 1, 0, 1 }, /* 2 */
+  { 1, 1, 1, 1, 0, 0, 1 }, /* 3 */
+  { 0, 1, 1, 0, 0, 1, 1 }, /* 4 */
+  { 1, 0, 1, 1, 0, 1, 1 }, /* 5 */
+  { 1, 0, 1, 1, 1, 1, 1 }, /* 6 */
+  { 1, 1, 1, 0, 0, 1, 0 }, /* 7 */
+  { 1, 1, 1, 1, 1, 1, 1 }, /* 8 */
+  { 1, 1, 1, 1, 0, 1, 1 }  /* 9 */         
+};
+
+void display_init(){
+  for(int x=0;x<7;x++){
+    pinMode(led[x], OUTPUT);
+  }
+  for(int y=1;y<=4;y++){
+    pinMode(com[y], HIGH);
+  }
+}
+
+void display_clear(){
+  for(int x=0;x<7;x++){
+    digitalWrite(led[x], HIGH); //공통양극, 음극이면 LOW일 때 꺼짐
+  }
+  for(int y=1;y<=4;y++){
+    digitalWrite(com[y], LOW); //공통양극, 음극이면 HIGH일 때 켜짐
+  }
+}
+
+void display_number(unsigned int n, unsigned int m){
+  display_clear();
+  if(0<=n&&n<=9 && 1<=m&&m<=4){
+    for(int x=0;x<7;x++){
+      digitalWrite(led[x], num[n][x] == 1 ? LOW : HIGH); // 공통양극
+    }
+    digitalWrite(com[m], HIGH); //공통양극
+  }
+}
+
+void display_numbers(unsigned int num){
+  unsigned int num_1000 = num/1000;
+  unsigned int num_100 = (num%1000)/100;
+  unsigned int num_10 = (num%100)/10;
+  unsigned int num_1 = num%10;
+
+  
+  display_number(num_1000, 1);
+  delay(1);
+  display_number(num_100, 2);
+  delay(1);
+  display_number(num_10, 3);
+  delay(1);
+  display_number(num_1, 4);
+  delay(1);
+}
+
+void setup() {
+  display_init();
+}
+
+void loop() {
+  static unsigned int num = 0;
+  num++;
+
+  unsigned int cnt = 0;
+  while(1) {
+    display_numbers(num); // 한 번 호출하는 데 0.004초 소요, 25번 num을 찍음, 0.1초
+    cnt++;
+    if(cnt == 25) break;
+  }
+}
